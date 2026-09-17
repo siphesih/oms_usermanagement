fab8fea21adce7788e672f2acd948325dfdc4c00
siphesih <maluleki01@gmail.com>
2026-09-17 15:14:39 +0200

    Initial commit

---
+++ src/main/java/fnb/oms_usermanagement/OmsUsermanagementApplication.java
@@ -0,0 +1,13 @@
+package fnb.oms_usermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmsUsermanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmsUsermanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fnb/oms_usermanagement/OmsUsermanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package fnb.oms_usermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmsUsermanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
